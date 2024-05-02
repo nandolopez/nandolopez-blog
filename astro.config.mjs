97d9aab1a987d155efd0b70ed514a000c73c3628
Nando López <fernanlm88@gmail.com>
2024-05-02 08:23:05 +0200

    Added logos and images for default in posts, replaced ../../ for @ in imports and replaced About page for CV

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nandolopez.github.io',
+  base:".",
+  
   build: {
     format: "file"
   },
